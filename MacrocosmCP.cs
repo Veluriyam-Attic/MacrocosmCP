61e4ee6091f246b22f0ae49871d79cdcf7da98ef
Henceforth <3412615165@qq.com>
2025-03-23 22:59:50 +0800

    为啥这么多本地化更改

--- MacrocosmCP.cs
+++ MacrocosmCP.cs
@@ -1,15 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace MacrocosmCP
 {
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class MacrocosmCP : Mod
 	{
 
-	}
+    }
+
+	public class MacrososmCNPlayer : ModPlayer
+	{
+        public override void OnEnterWorld()
+        {
+            //Main.NewText("Macrocosm汉化繁简转换测试中！/n在模组配置中切换！", Color.Orange);
+        }
+    }
 }
