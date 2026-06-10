6456fa06baf2a8a5ae95504c9600f8c2a23f86fc
Henceforth <3412615165@qq.com>
2026-06-11 03:06:13 +0800

    Updated

--- obj/Debug/net8.0/MacrocosmCP.AssemblyInfo.cs
+++ obj/Debug/net8.0/MacrocosmCP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MacrocosmCP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8220729cd99b96df0b6559b4a3f42b888580a22c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a19de07bf603180df3560f2283fe52f45527c3dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MacrocosmCP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MacrocosmCP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
